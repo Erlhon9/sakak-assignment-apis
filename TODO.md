95b4690c9f958c6ee73b0a9ffa50e0d48465aa7d
Erlhon9 <hongsbus@gmail.com>
2024-03-19 18:56:29 +0900

    feat: migrate data

---
+++ TODO.md
@@ -0,0 +1,3 @@
+1. DATA Migration
+
+- [] float데이터 처리중 1g 미만은 어떻게 처리할 지 정하기
--- django_project/settings/base.py
+++ django_project/settings/base.py
@@ -88,3 +88,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+FILE_PATH = BASE_DIR.parent / "media"
