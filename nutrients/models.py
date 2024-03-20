86830bc5a35b31079ff818d099faa318dfad64e6
Erlhon9 <hongsbus@gmail.com>
2024-03-20 17:06:41 +0900

    feat: add index to db

--- nutrients/models.py
+++ nutrients/models.py
@@ -3,11 +3,11 @@ from django.db import models
 
 class FoodNutrientFact(models.Model):
     id = models.AutoField(primary_key=True, help_text='번호')
-    food_cd = models.CharField(max_length=8, help_text='식품코드')
+    food_cd = models.CharField(max_length=8, help_text='식품코드', db_index=True)
     group_name = models.CharField(max_length=20, help_text='식품군')
-    food_name = models.CharField(max_length=100, help_text='식품이름')
-    research_year = models.CharField(max_length=4, help_text='조사년도')
-    maker_name = models.CharField(max_length=100, help_text='지역/제조사')
+    food_name = models.CharField(max_length=100, help_text='식품이름', db_index=True)
+    research_year = models.CharField(max_length=4, help_text='조사년도', db_index=True)
+    maker_name = models.CharField(max_length=100, help_text='지역/제조사', db_index=True)
     ref_name = models.CharField(max_length=100, help_text='자료출처')
     serving_size = models.FloatField(help_text='1회 제공량')
     calorie = models.FloatField(help_text='열량(kcal)(1회제공량당)', null=True, default=0.0)
